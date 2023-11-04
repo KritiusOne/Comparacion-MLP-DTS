e4aaa2cfa9c216396748d1ab34498f0b02f358eb
Julio Rodríguez <julio10489@gmail.com>
2023-11-04 18:25:51 -0500

    Inicializacion del DTS y MLP, llamado al dataset

---
+++ DTS.py
@@ -0,0 +1,10 @@
+# print("Soy el Arbol de desicion")
+from sklearn.neural_network import MLPClassifier
+import pandas as pd
+
+data = pd.read_csv("Metabolic Syndrome.csv", header=None)
+x = data.iloc[:, 1:]
+y = data.iloc[:, 12]
+print(x)
+print("----------------------------")
+print(y)
