ac26bd9a7fb088155cb335934e613ef39f191dc0
Julio Rodríguez <julio10489@gmail.com>
2023-11-05 01:25:07 -0500

    Actualizado: DTS, implementacion basica del Arbol de desicion

--- DTS.py
+++ DTS.py
@@ -1,3 +1,14 @@
 from dataTransform import cleanData
+from dataTransform import selection_testAndQuality
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+import numpy as np
 
 data = cleanData()
+x, y, trainToX, testToX, trainToY, testToY = selection_testAndQuality(data)
+
+DTS = DecisionTreeClassifier(max_depth=6, random_state=0)
+score_DTS = cross_val_score(DTS, x, y, cv=5)
+acurracy_DTS = round(np.mean(score_DTS), 5) * 100
+
+print("La presición promedio del DTS fue: ", acurracy_DTS, "%")
